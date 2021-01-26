92544f61b06e5b9f7f8dec7912fe8d7ba4c5dd5f
Kurt Mohler <kmohler@quansight.com>
2021-01-26 16:27:17 -0600

    Add example 4 and fix naming/organization

--- examples.cpp
+++ examples.cpp
@@ -1,8 +1,9 @@
-//#include <torch/torch.h>
 #include <iostream>
+#include <cassert>
+#include <ATen/ATen.h>
 #include <ATen/native/TensorIterator.h>
 #include <ATen/native/cpu/Loops.h>
-#include <ATen/ATen.h>
+#include <ATen/native/ReduceOpsUtils.h>
 
 void example1() {
   at::Tensor a = at::ones({10});
@@ -10,19 +11,20 @@ void example1() {
   at::Tensor out = at::zeros({0});
   std::cout
     << "\n==========\n"
-    << "example1()\n"
-    << "=========="
+    << "example1:"
     << std::endl;
 
   //======== Start blog post code =========
   at::TensorIteratorConfig iter_config;
-  iter_config.add_output(out)
+  iter_config
+    .add_output(out)
     .add_input(a)
     .add_input(b);
 
   auto iter = iter_config.build();
   //======== End blog post code ===========
-  std:: cout << "\nno output" << std::endl;
+
+  std::cout << "PASS" << std::endl;
 }
 
 void example2() {
@@ -30,13 +32,13 @@ void example2() {
   at::Tensor out = at::zeros({10});
   std::cout
     << "\n==========\n"
-    << "example2()\n"
-    << "=========="
+    << "example2:"
     << std::endl;
 
   //======== Start blog post code =========
   at::TensorIteratorConfig iter_config;
-  iter_config.add_output(out)
+  iter_config
+    .add_output(out)
     .add_input(a)
 
     // call if out was already allocated
@@ -49,22 +51,23 @@ void example2() {
 
   // Copies data from input into output
   auto copy_loop = [&](char** data, const int64_t* strides, int64_t n) {
+
     // assume float data type for this example
-    float* out_data_bytes = reinterpret_cast<float*>(data[0]);
-    float* in_data_bytes = reinterpret_cast<float*>(data[1]);
+    float* out_data = reinterpret_cast<float*>(data[0]);
+    float* in_data = reinterpret_cast<float*>(data[1]);
 
     for (int i = 0; i < n; i++) {
-      *out_data_bytes += *in_data_bytes;
-      out_data_bytes++;
-      in_data_bytes++;
+      *out_data += *in_data;
+      out_data++;
+      in_data++;
     }
   };
 
   iter.for_each(copy_loop);
   //======== End blog post code ===========
-  //
-  std::cout << "\na:\n" << a << std::endl;
-  std::cout << "\nout:\n" << out << std::endl;
+
+  assert((out == a).all().item<bool>());
+  std::cout << "PASS" << std::endl;
 }
 
 void example3() {
@@ -73,30 +76,73 @@ void example3() {
   at::Tensor c = at::zeros({0});
   std::cout
     << "\n==========\n"
-    << "example3()\n"
-    << "=========="
+    << "example3:"
     << std::endl;
 
   //======== Start blog post code =========
   at::TensorIteratorConfig iter_config;
-  iter_config.add_output(c)
+  iter_config
+    .add_output(c)
     .add_input(a)
     .add_input(b);
 
   auto iter = iter_config.build();
+
+  // Element-wise add
   at::native::cpu_kernel(iter, [] (float a, float b) -> float {
     return a + b;
   });
   //======== End blog post code ===========
 
-  std::cout << "\na:\n" << a << std::endl;
-  std::cout << "\nb:\n" << b << std::endl;
-  std::cout << "\nc:\n" << c << std::endl;
+  assert((a + b).eq(c).all().item<bool>());
+  std::cout << "PASS" << std::endl;
+}
+
+void example4() {
+  at::Tensor self = at::randn({10});
+  std::cout
+    << "\n==========\n"
+    << "example4:"
+    << std::endl;
+
+  //======== Start blog post code =========
+  // `make_reduction` will allocate result Tensor for us, so we
+  // can leave it undefined
+  at::Tensor result;
+
+  int64_t dim = 0;
+  bool keepdim = false;
+  auto iter = at::native::make_reduction(
+    "sum_reduce",
+    result,
+    self,
+    dim,
+    keepdim,
+    self.scalar_type());
+
+  // Sum reduce data from input into output
+  auto sum_reduce_loop = [&](char** data, const int64_t* strides, int64_t n) {
+    // assume float data type for this example
+    float* out_data = reinterpret_cast<float*>(data[0]);
+    float* in_data = reinterpret_cast<float*>(data[1]);
+
+    *out_data = 0;
+    for (int i = 0; i < n; i++) {
+      *out_data += *in_data;
+      in_data++;
+    }
+  };
+
+  iter.for_each(sum_reduce_loop);
+  //======== End blog post code ===========
+
+  assert((self.sum(dim, keepdim) - result).abs().lt(0.00001).all().item<bool>());
+  std::cout << "PASS" << std::endl;
 }
 
 int main() {
-  at::manual_seed(0);
   example1();
   example2();
   example3();
+  example4();
 }
